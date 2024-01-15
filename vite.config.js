e10a0b4f4840c84871ac0b09c45f4233c9480c1e
曹绪培 <2877406366@qq.com>
2024-01-15 19:15:02 +0800

    文件管理

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://182.92.201.19:8080',
+        target: 'http://182.92.201.19:8080/',
         // target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
